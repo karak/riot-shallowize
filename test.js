838695d796583e8cb8ad4750f7441a311237fb98
karak <karak97@gmail.com>
2018-04-10 02:47:32 +0900

    refactor: Exclude setup test to a class.

--- test.js
+++ test.js
@@ -5,39 +5,66 @@ const riot = shallowize(_riot);
 riot.tag2('inner-tag', 'Hello!, {opts.data}!', '', '', () => {});
 riot.tag2('tag', '<inner-tag data={"test"}>(child)</inner-tag>', '', '', () => {});
 
+/** Test harness to setup a single tag */
+class DomEnv {
+  constructor(tagName) {
+    this._tagName = tagName;
+  }
+
+  createElement() {
+    const div = document.createElement(this._tagName);
+    document.body.appendChild(div);
+    this._container = div;
+  }
+
+  mount(opts) {
+    return this._rootTag = riot.mount(this._tagName, opts)[0];
+  }
+
+  shallow(selector, opts) {
+    if (typeof selector === 'string') {
+      return this._rootTag = riot.shallow(selector, this._tagName, opts)[0];
+    } else {
+      return this._rootTag = riot.shallow(this._tagName, opts)[0];
+    }
+  }
+
+  unmount() {
+    if (this._rootTag) {
+      this._rootTag.unmount();
+    } else if (this._container) {
+      document.body.removeChild(this._container)
+      delete this._container;
+    }
+  }
+}
+
 describe('test', () => {
+  const dom = new DomEnv('tag');
+
+  beforeEach(() => dom.createElement())
+  afterEach(() => dom.unmount());
+
   describe('deep', () => {
     it('mount works', () => {
-      const div = document.createElement('tag');
-      document.body.appendChild(div);
-
-      const rootTag = riot.mount('tag')[0];
+      const rootTag = dom.mount();
 
       expect(rootTag.root.querySelectorAll('inner-tag').length).toBe(1)
       expect(rootTag.root.querySelector('inner-tag').innerHTML).toBe('Hello!, test!')
-
-      rootTag.unmount();
     });
   });
 
   describe('shallow', () => {
     it('shallow works', () => {
-      const div = document.createElement('tag');
-      document.body.appendChild(div);
-
-      const rootTag = riot.shallow('tag')[0];
+      const rootTag = dom.shallow();
 
       expect(rootTag.root.querySelectorAll('inner-tag').length).toBe(1)
       expect(rootTag.root.querySelector('inner-tag').getAttribute('data')).toBe('test')
       expect(rootTag.root.querySelector('inner-tag').innerHTML).toBe('(child)')
-
-      rootTag.unmount();
     });
 
     it('with tagName opts', () => {
-      const div = document.createElement('tag');
-      document.body.appendChild(div);
-      const rootTag = riot.shallow('*', 'tag')[0];
+      const rootTag = dom.shallow('*');
 
       expect(rootTag.root.querySelectorAll('inner-tag').length).toBe(1)
     });
